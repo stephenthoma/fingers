a9cdc7230034b480150d260bda200f4ced085be1
Stephen Thoma <sthoma4@gmail.com>
2016-11-07 15:35:08 -0700

    More functional flash lso.

---
+++ Storage.as
@@ -0,0 +1,86 @@
+package {
+
+import flash.display.Sprite;
+import flash.external.ExternalInterface;
+import flash.text.TextField;
+import flash.system.Security;
+import flash.net.SharedObject;
+import flash.net.SharedObjectFlushStatus;
+
+public class Storage extends Sprite {
+
+  // The JS callback functions should all be on a global variable at SwfStore.<namespace>.<function name>
+  private var jsNamespace:String = "Finger.fingerstore.";
+
+  private var namespaceCheck:RegExp = /^[a-z0-9_\/]+$/i;
+  private var logText:TextField;
+
+  public function Storage() {
+    if (!ExternalInterface.available) {
+      return;
+    }
+    ExternalInterface.marshallExceptions = true;
+
+    if (this.loaderInfo.parameters.namespace && !namespaceCheck.test(this.loaderInfo.parameters.namespace)) {
+      return;
+    }
+
+    // Get external log function
+    if (this.loaderInfo.parameters.namespace) {
+      jsNamespace = "Finger['" + this.loaderInfo.parameters.namespace+ "'].";
+    }
+
+    Security.allowDomain("*");
+    Security.allowInsecureDomain("*");
+
+    var dataStore:SharedObject = SharedObject.getLocal(this.loaderInfo.parameters.namespace);
+
+    // Setup external interface
+    ExternalInterface.addCallback("set", setValue);
+    ExternalInterface.addCallback("get", getValue);
+
+    ExternalInterface.call(jsNamespace + "swfLoad");
+  }
+
+  private function onError(errStr:String):void {
+    if (ExternalInterface.available) {
+      ExternalInterface.call(jsNamespace + "swfError", errStr);
+    }
+  }
+
+  // Store lso data
+  private function setValue(key:String, val:*):void {
+    var dataStore:SharedObject = SharedObject.getLocal(this.loaderInfo.parameters.namespace);
+    log('Setting ' + key + ' = ' + val);
+    dataStore.data[key] = val;
+    flush(dataStore);
+  }
+
+  // Get lso data
+  private function getValue(key:String):String {
+    var dataStore:SharedObject = SharedObject.getLocal(this.loaderInfo.parameters.namespace);
+    if (dataStore.data.hasOwnProperty(key)) {
+      var val:String = dataStore.data[key];
+      return val;
+    }
+    else {
+      return null;
+    }
+  }
+
+  // Flush dataStore changes
+  private function flush(dataStore:SharedObject):void {
+    var flushStatus:String = null;
+    try {
+      flushStatus = dataStore.flush(10000);
+    } catch (error:Error) {
+      onError("Unable to write SharedObject to disk: " + error.message);
+    }
+  }
+
+  // Send logs to js
+  private function log(str:String):void {
+    ExternalInterface.call(jsNamespace + "swfLog", 'debug', 'swfStore', 'FLASH: '+ str);
+  }
+}
+}
--- src/fingers.js
+++ src/fingers.js
@@ -6,19 +6,31 @@ var swfobject = require('swfobject');
 
 function Finger () {
   if (!(this instanceof Finger)) return new Finger();
-}
+
+  if (this.hasFlash()) {
+    this.swf_ready = false;
+    this.swf_container_id = "fingerstore";
+    Finger[this.swf_container_id] = this;
+
+    this.embedSWFObject(this.swf_container_id);
+    this._timeout = setTimeout(function() {
+      Finger[this.swf_container_id].swfError(new Error("Failed to load SWF."), 'js');
+    }, 5000);
+  }
+};
 
 Finger.prototype.getPrint = function() {
   var metrics = {};
-  metrics.language = this.getLanguage();
+  metrics.language = this.getLanguages();
   metrics.platform = this.getPlatform();
   metrics.timezone = this.getTimezone();
   metrics.resolution = this.getScreenResolution();
+  metrics.flash = this.getFlashVersion();
 
   return murmur.hash128(JSON.stringify(metrics)).hex();
 };
 
-Finger.prototype.getLanguage = function() {
+Finger.prototype.getLanguages = function() {
   if (navigator.language) {
     return navigator.language.toLowerCase();
   }
@@ -46,19 +58,68 @@ Finger.prototype.hasFlash = function() {
 };
 
 Finger.prototype.getFlashVersion = function() {
+  var res = {};
   if (this.hasFlash()) {
-    return {flash_ver: swfobject.getFlashPlayerVersion()};
+    res.flash_ver = swfobject.getFlashPlayerVersion();
+  }
+  else {
+    res.flash_ver = null;
+  }
+  return res;
+};
+
+Finger.prototype.embedSWFObject = function(container_id) {
+  var swf_div = document.createElement("object");
+  swf_div.setAttribute("id", container_id);
+  swf_div.position = "absolute";
+  swf_div.style.top = "-1000px";
+  swf_div.style.left = "-1000px";
+  document.body.appendChild(swf_div);
+
+  var FlashVars = {namespace: container_id};
+  var params = {allowScriptAccess: "always"}
+  swfobject.embedSWF("storage.swf", container_id, "1", "1", "9.0.0", false, FlashVars, params);
+};
+
+Finger.prototype.setFlashLSO = function(value) {
+  this._checkSwfReady();
+  window[this.swf_container_id].set('finger', value);
+};
+
+Finger.prototype.getFlashLSO = function() {
+  this._checkSwfReady();
+  return window[this.swf_container_id].get('finger');
+};
+
+Finger.prototype._checkSwfReady = function() {
+  if (!this.swf_ready) {
+    throw "SWF connection not yet established."
   }
 };
 
-Finger.prototype.setFlashLSO = function() {
-  var swf_div = document.createElement("div");
-  swf_div.setAttribute("id", "swfcontainer");
-  document.body.appendChild(swf_div);
-
-  var flashvars = {everdata: "55555"};
-  swfobject.embedSWF("lso.swf", "swfcontainer", "1", "1", "9.0.0", false, flashvars);
+Finger.prototype.swfReady = function() {
+  this.setFlashLSO('vallll');
+  console.log(this.getFlashLSO());
 };
+
+Finger.prototype.swfLoad = function() {
+  var that = this;
+  setTimeout(function() {
+    clearTimeout(that._timeout);
+    that.swf_ready = true;
+    that.swfReady();
+  }, 0);
+};
+
+Finger.prototype.swfLog = function(type, source, msg) {
+  console.log(msg);
+};
+
+Finger.prototype.swfError = function(err, source) {
+  console.log(err);
+};
+
+window.Finger = Finger;
 
 var finger = new Finger();
 console.log(finger.getPrint());
